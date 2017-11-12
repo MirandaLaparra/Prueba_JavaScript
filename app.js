c3f6162446e7f360b96d046a9584e08b2f0826bb
MirandaLaparra <mmlaparra_87@hotmail.com>
2017-11-11 22:39:37 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var calculadora = {
-
-	visor: document.getElementById("display"),
+visor: document.getElementById("display"),
 valorVisor: "0",
 operacion: "",
 primerValor: 0,
@@ -9,38 +8,30 @@ ultimoValor: 0,
 resultado: 0,
 auxTeclaIgual: false,
 
-init: function() {
-	calculadora.animacionTeclas;
-	calculadora.agregarValores;
-}
+init: (function() {
+	this.agregarValores();
+}),
 
-animacionTeclas: function ("tecla"){
-document.getElementById("tecla").style.transform="scale(0.9)";
-
-setTimeout(function() {document.getElementById(tecla).style.transform="scale(1)";}, 200);
-}
-
-		agregarValores: function (tecla){
-			document.getElementById("0").addEventListener("click", function() {calculadora.Numero("0") ;});
-			document.getElementById("1").addEventListener("click", function() {calculadora.Numero("1");});
-			document.getElementById("2").addEventListener("click", function() {calculadora.Numero("2");});
-			document.getElementById("3").addEventListener("click", function() {calculadora.Numero("3");});
-			document.getElementById("4").addEventListener("click", function() {calculadora.Numero("4");});
-			document.getElementById("5").addEventListener("click", function() {calculadora.Numero("5");});
-			document.getElementById("6").addEventListener("click", function() {calculadora.Numero("6");});
-			document.getElementById("7").addEventListener("click", function() {calculadora.Numero("7");});
-			document.getElementById("8").addEventListener("click", function() {calculadora.Numero("8");});
-			document.getElementById("9").addEventListener("click", function() {calculadora.Numero("9");});
-			document.getElementById("on").addEventListener("click", function() {calculadora.borrar();});
-			document.getElementById("sign").addEventListener("click", function() {calculadora.cambiarSigno();});
-			document.getElementById("punto").addEventListener("click", function() {calculadora.puntoDecimal();});
-			document.getElementById("igual").addEventListener("click", function() {calculadora.Resultado();});
-			document.getElementById("dividido").addEventListener("click", function() {calculadora.Operacion("/");});
-			document.getElementById("por").addEventListener("click", function() {calculadora.Operacion("*");});
-			document.getElementById("menos").addEventListener("click", function() {calculadora.Operacion("-");});
-			document.getElementById("mas").addEventListener("click", function() {calculadora.Operacion("+");});
-			this.animacionTeclas;
-		},
+agregarValores: function (){
+	document.getElementById("0").addEventListener("click", function() {calculadora.ingresoNumero("0");});
+	document.getElementById("1").addEventListener("click", function() {calculadora.ingresoNumero("1");});
+	document.getElementById("2").addEventListener("click", function() {calculadora.ingresoNumero("2");});
+	document.getElementById("3").addEventListener("click", function() {calculadora.ingresoNumero("3");});
+	document.getElementById("4").addEventListener("click", function() {calculadora.ingresoNumero("4");});
+	document.getElementById("5").addEventListener("click", function() {calculadora.ingresoNumero("5");});
+	document.getElementById("6").addEventListener("click", function() {calculadora.ingresoNumero("6");});
+	document.getElementById("7").addEventListener("click", function() {calculadora.ingresoNumero("7");});
+	document.getElementById("8").addEventListener("click", function() {calculadora.ingresoNumero("8");});
+	document.getElementById("9").addEventListener("click", function() {calculadora.ingresoNumero("9");});
+	document.getElementById("on").addEventListener("click", function() {calculadora.borrarVisor();});
+	document.getElementById("sign").addEventListener("click", function() {calculadora.cambiarSigno();});
+	document.getElementById("punto").addEventListener("click", function() {calculadora.puntoDecimal();});
+	document.getElementById("igual").addEventListener("click", function() {calculadora.verResultado();});
+	document.getElementById("dividido").addEventListener("click", function() {calculadora.ingresarOperacion("/");});
+	document.getElementById("por").addEventListener("click", function() {calculadora.ingresarOperacion("*");});
+	document.getElementById("menos").addEventListener("click", function() {calculadora.ingresarOperacion("-");});
+	document.getElementById("mas").addEventListener("click", function() {calculadora.ingresarOperacion("+");});
+	},
 
 //6
 		borrarVisor: function(){
@@ -52,9 +43,8 @@ setTimeout(function() {document.getElementById(tecla).style.transform="scale(1)"
 			this.Operación = "";
 			this.auxTeclaIgual = false;
 			this.ultimoValor = 0;
-			this.updateVisor();
+			this.actualizarVisor();
 		},
-
 //7
 		ingresoDecimal: function(){
 			if (this.valorVisor.indexOf(".")== -1) {
@@ -63,10 +53,9 @@ setTimeout(function() {document.getElementById(tecla).style.transform="scale(1)"
 				} else {
 					this.valorVisor = this.valorVisor + ".";
 				}
-				this.updateVisor();
+				this.actualizarVisor();
 			}
 		},
-
 //8
 		cambiarSigno: function(){
 			if (this.valorVisor !="0") {
@@ -78,60 +67,47 @@ setTimeout(function() {document.getElementById(tecla).style.transform="scale(1)"
 				}
 			this.valorVisor = "";
 			this.valorVisor = aux;
-			this.updateVisor();
+			this.actualizarVisor();
 			}
 		},
 
-
-
 		ingresoNumero: function(valor){
 			if (this.valorVisor.length < 8) {
-
 				if (this.valorVisor=="0") {
 					this.valorVisor = "";
 					this.valorVisor = this.valorVisor + valor;
 				} else {
-					this.valorVisor = this.valorVisor + valor;
+					this.valorVisor = + valor;
 				}
-			this.updateVisor();
+			this.actualizarVisor();
 			}
 		},
 
-		ingresoOperacion: function(oper){
+		ingresarOperacion: function(oper){
 			this.primerValor = parseFloat(this.valorVisor);
 			this.valorVisor = "";
 			this.operacion = oper;
 			this.auxTeclaIgual = false;
-			this.updateVisor();
+			this.actualizarVisor();
 		},
 
 		verResultado: function(){
-
 			if(!this.auxTeclaIgual){
 				this.segundoValor = parseFloat(this.valorVisor);
 				this.ultimoValor = this.segundoValor;
-
 				this.realizarOperacion(this.primerValor, this.segundoValor, this.operacion);
-
 			} else {
 			this.realizarOperacion(this.primerValor, this.ultimoValor, this.operacion);
 			}
-
 			this.primerValor = this.resultado;
-
 			this.valorVisor = "";
-
-
 			if (this.resultado.toString().length < 9){
 				this.valorVisor = this.resultado.toString();
 			} else {
 				this.valorVisor = this.resultado.toString().slice(0,8) + "...";
 			}
-
-
 			this.auxTeclaIgual = true;
-			this.updateVisor();
-
+			this.actualizarVisor();
 		},
 
 		realizarOperacion: function(primerValor, segundoValor, operacion){
@@ -150,7 +126,10 @@ setTimeout(function() {document.getElementById(tecla).style.transform="scale(1)"
 				break;
 			}
 		},
-
+		actualizarVisor: function(){
+			this.visor.innerHTML = this.valorVisor;
 		}
+
+		};
 
 		calculadora.init();
